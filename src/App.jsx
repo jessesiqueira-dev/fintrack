902258467f9420369957e6a906b8660112446fbd
jessesiqueira-dev <jsiqueira397vendas@gmail.com>
2026-08-01 15:06:23 -0300

    chore: set up Tailwind CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 const App = () => {
-  return <h1>FinTrack</h1>;
+  return <h1 className="text-red-500">FinTrack</h1>;
 };
 
 export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailw  indcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
