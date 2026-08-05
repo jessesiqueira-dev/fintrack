7f31d3e3f7822ec662eb63bc60faf3184355522b
jessesiqueira-dev <jsiqueira397vendas@gmail.com>
2026-08-05 19:19:38 -0300

    feat: add initial sign up page

--- src/pages/signup.jsx
+++ src/pages/signup.jsx
@@ -1,5 +1,46 @@
+import { Button } from '@/components/ui/button'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
+import { Input } from '@/components/ui/input'
+
 const SignUpPage = () => {
-  return <h1>Sign Up Page</h1>
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center gap-4 px-4">
+      <Card className="w-full max-w-[500px]">
+        <CardHeader>
+          <CardTitle>Crie uma nova conta</CardTitle>
+          <CardDescription>Insira seus dados abaixo.</CardDescription>
+        </CardHeader>
+
+        <CardContent className="space-y-4">
+          <Input placeholder="Digite seu nome" />
+          <Input placeholder="Digite seu sobrenome" />
+          <Input type="email" placeholder="Digite seu e-mail" />
+          <Input type="password" placeholder="Digite sua senha" />
+        </CardContent>
+
+        <CardFooter>
+          <Button type="submit" className="w-full">
+            Criar conta
+          </Button>
+        </CardFooter>
+      </Card>
+
+      <div className="flex items-center justify-center">
+        <p className="text-center opacity-50">Já tem uma conta?</p>
+
+        <Button variant="link" asChild>
+          <a href="/login">Faça login</a>
+        </Button>
+      </div>
+    </div>
+  )
 }
 
 export default SignUpPage
