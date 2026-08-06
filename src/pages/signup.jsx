9b6dd96a08c8dd192618c6ff8fbac0764e0d06a4
jessesiqueira-dev <jsiqueira397vendas@gmail.com>
2026-08-06 14:58:24 -0300

    feat: add validation to signup form

--- src/pages/signup.jsx
+++ src/pages/signup.jsx
@@ -1,6 +1,6 @@
 import { zodResolver } from '@hookform/resolvers/zod'
 import { useForm } from 'react-hook-form'
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router'
 import { z } from 'zod'
 
 import { Button } from '@/components/ui/button'
