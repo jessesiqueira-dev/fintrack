f7424455862f84b51dae44b2a26538f525c625b9
jessesiqueira-dev <jsiqueira397vendas@gmail.com>
2026-08-05 22:08:14 -0300

    feat: add terms acceptance checkbox

--- src/pages/signup.jsx
+++ src/pages/signup.jsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router'
+
 import { Button } from '@/components/ui/button'
 import {
   Card,
@@ -7,42 +9,51 @@ import {
   CardHeader,
   CardTitle,
 } from '@/components/ui/card'
+import { Checkbox } from '@/components/ui/checkbox'
 import { Input } from '@/components/ui/input'
 import PasswordInput from '@/components/ui/password-input'
 
-const SignUpPage = () => {
+const SignupPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center gap-4 px-4">
-      <Card className="w-full max-w-[500px]">
+    <div className="flex h-screen w-screen flex-col items-center justify-center gap-3">
+      <Card className="w-[500px]">
         <CardHeader>
-          <CardTitle>Crie uma nova conta</CardTitle>
-          <CardDescription>Insira seus dados abaixo.</CardDescription>
+          <CardTitle>Crie a sua conta</CardTitle>
+          <CardDescription>Insira os seus dados abaixo.</CardDescription>
         </CardHeader>
-
         <CardContent className="space-y-4">
           <Input placeholder="Digite seu nome" />
           <Input placeholder="Digite seu sobrenome" />
-          <Input type="email" placeholder="Digite seu e-mail" />
+          <Input placeholder="Digite seu e-mail" />
           <PasswordInput />
           <PasswordInput placeholder="Digite sua senha novamente" />
+          <div className="items-top flex space-x-2">
+            <Checkbox id="terms" />
+            <div className="grid gap-1.5 leading-none">
+              <label
+                htmlFor="terms"
+                className="text-xs text-muted-foreground opacity-75"
+              >
+                Ao clicar em “Criar conta”, você aceita{' '}
+                <a href="#" className="text-white underline">
+                  nosso termo de uso e política de privacidade.
+                </a>
+              </label>
+            </div>
+          </div>
         </CardContent>
-
         <CardFooter>
-          <Button type="submit" className="w-full">
-            Criar conta
-          </Button>
+          <Button className="w-full">Criar conta</Button>
         </CardFooter>
       </Card>
-
       <div className="flex items-center justify-center">
-        <p className="text-center opacity-50">Já tem uma conta?</p>
-
+        <p className="text-center opacity-50">Já possui uma conta?</p>
         <Button variant="link" asChild>
-          <a href="/login">Faça login</a>
+          <Link to="/login">Faça login</Link>
         </Button>
       </div>
     </div>
   )
 }
 
-export default SignUpPage
+export default SignupPage
