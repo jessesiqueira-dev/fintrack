4e0beb890cc29304f94719db803df32f909584ba
jessesiqueira-dev <jsiqueira397vendas@gmail.com>
2026-08-10 21:28:19 -0300

    refactor: simplify authorization header syntax

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -2,19 +2,24 @@ import axios from 'axios'
 
 import { LOCAL_STORAGE_ACCESS_TOKEN_KEY } from '@/constants/local-storage'
 
-export const protectedApi = axios.create({
-  baseURL: 'https://fullstackclub-finance-dashboard-api.onrender.com/api', // Replace with your API base URL
-  timeout: 10000, // Set a timeout for requests (in milliseconds)
-})
+const baseURL = 'https://fullstackclub-finance-dashboard-api.onrender.com/api'
+
 export const publicApi = axios.create({
-  baseURL: 'https://fullstackclub-finance-dashboard-api.onrender.com/api', // Replace with your API base URL
-  timeout: 10000, // Set a timeout for requests (in milliseconds)
+  baseURL,
+  timeout: 10000,
 })
+
+export const protectedApi = axios.create({
+  baseURL,
+  timeout: 10000,
+})
+
 protectedApi.interceptors.request.use((request) => {
   const accessToken = localStorage.getItem(LOCAL_STORAGE_ACCESS_TOKEN_KEY)
-  if (!accessToken) {
-    return request
+
+  if (accessToken) {
+    request.headers.Authorization = `Bearer ${accessToken}`
   }
-  request.headers['Authorization'] = `Bearer ${accessToken}`
+
   return request
 })
