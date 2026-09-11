3d97746f4c3f1a5ba90e75916e847d65d9e2cb03
jessesiqueira-dev <jsiqueira397vendas@gmail.com>
2026-09-11 20:23:09 -0300

    feat: initial create transaction dialog and form

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,10 +1,9 @@
-import { PlusIcon } from 'lucide-react'
 import { Navigate } from 'react-router'
 
+import AddTransactionButton from '@/components/add-transaction-button'
 import Balance from '@/components/balance'
 import DateSelection from '@/components/date-selection'
 import Header from '@/components/header'
-import { Button } from '@/components/ui/button'
 import { useAuthContext } from '@/contexts/auth'
 
 const HomePage = () => {
@@ -22,10 +21,7 @@ const HomePage = () => {
           <h2 className="text-2xl font-bold">Dashboard</h2>
           <div className="flex items-center gap-2">
             <DateSelection />
-            <Button>
-              <PlusIcon />
-              Nova transação
-            </Button>
+            <AddTransactionButton />
             {/* SELETOR DATA E BOTAO DE NOVA TRANSACAO */}
           </div>
         </div>
