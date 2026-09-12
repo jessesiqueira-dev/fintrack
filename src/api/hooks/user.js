79ff8dc2bb5c42e2638b633fbb6021e0126291f4
jessesiqueira-dev <jsiqueira397vendas@gmail.com>
2026-09-12 20:37:05 -0300

    feat: add hooks that gets user balance

---
+++ src/api/hooks/user.js
@@ -0,0 +1,23 @@
+import { useQuery } from '@tanstack/react-query'
+
+import { UserService } from '@/api/services/user'
+import { useAuthContext } from '@/contexts/auth'
+
+export const getUserBalanceQueryKey = ({ userId, from, to }) => {
+  if (!from || !to) {
+    return ['balance', userId]
+  }
+  return ['balance', userId, from, to]
+}
+
+export const useGetUserBalance = ({ from, to }) => {
+  const { user } = useAuthContext()
+  return useQuery({
+    queryKey: getUserBalanceQueryKey({ userId: user.id, from, to }),
+    queryFn: () => {
+      return UserService.getBalance({ from, to })
+    },
+    staleTime: 1000 * 60 * 5, // 5 minutes
+    enabled: Boolean(from) && Boolean(to) && Boolean(user.id),
+  })
+}
--- src/components/balance.jsx
+++ src/components/balance.jsx
@@ -1,4 +1,3 @@
-import { useQuery } from '@tanstack/react-query'
 import {
   PiggyBankIcon,
   TrendingDownIcon,
@@ -7,25 +6,15 @@ import {
 } from 'lucide-react'
 import { useSearchParams } from 'react-router'
 
-import { useAuthContext } from '@/contexts/auth'
-import { UserService } from '@/services/user'
+import { useGetUserBalance } from '@/api/hooks/user'
 
 import BalanceItem from './balance-item'
 
 const Balance = () => {
   const [searchParams] = useSearchParams()
-  const { user } = useAuthContext()
   const from = searchParams.get('from') // YYYY-MM-DD
   const to = searchParams.get('to') // YYYY-MM-DD
-  const { data } = useQuery({
-    queryKey: ['balance', user.id, from, to],
-    queryFn: () => {
-      return UserService.getBalance({ from, to })
-    },
-    staleTime: 1000 * 60 * 5, // 5 minutos
-    enabled: Boolean(from) && Boolean(to) && Boolean(user.id), // só vai rodar a query se eu tiver o "from" e o "to"
-  })
-  console.log({ data })
+  const { data } = useGetUserBalance({ from, to })
   return (
     <div className="grid grid-cols-2 grid-rows-2 gap-6">
       <BalanceItem
