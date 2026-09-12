c7b43a6b658183cbdd3847a3479c19fcb01cd79c
jessesiqueira-dev <jsiqueira397vendas@gmail.com>
2026-09-12 19:50:02 -0300

    chore: add react query devtools

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import './index.css'
 
 import { QueryClient } from '@tanstack/react-query'
 import { QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Route, Routes } from 'react-router'
@@ -28,6 +29,7 @@ createRoot(document.getElementById('root')).render(
           </Routes>
         </BrowserRouter>
         <Toaster />
+        <ReactQueryDevtools initialIsOpen={false} />
       </AuthContextProvider>
     </QueryClientProvider>
   </StrictMode>
