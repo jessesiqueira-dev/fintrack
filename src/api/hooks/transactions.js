9fbcd32a2a3d344864011ae37b330f14f9530dc0
jessesiqueira-dev <jsiqueira397vendas@gmail.com>
2026-09-12 21:24:19 -0300

    refactor: add create transaction hook

---
+++ src/api/hooks/transactions.js
@@ -0,0 +1,23 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+
+import { useAuthContext } from '@/contexts/auth'
+
+import { TransactionService } from '../services/transaction'
+import { getUserBalanceQueryKey } from './user'
+
+export const createTransactionMutationKey = ['createTransaction']
+
+export const useCreateTransaction = () => {
+  const queryClient = useQueryClient()
+  const { user } = useAuthContext()
+  return useMutation({
+    mutationKey: createTransactionMutationKey,
+    mutationFn: (input) => TransactionService.create(input),
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: getUserBalanceQueryKey({ userId: user.id }),
+        exact: false, // padrão
+      })
+    },
+  })
+}
--- src/components/add-transaction-button.jsx
+++ src/components/add-transaction-button.jsx
@@ -1,5 +1,4 @@
 import { zodResolver } from '@hookform/resolvers/zod'
-import { useMutation, useQueryClient } from '@tanstack/react-query'
 import {
   Loader2Icon,
   PiggyBankIcon,
@@ -13,8 +12,7 @@ import { NumericFormat } from 'react-number-format'
 import { toast } from 'sonner'
 import { z } from 'zod'
 
-import { getUserBalanceQueryKey } from '@/api/hooks/user'
-import { TransactionService } from '@/api/services/transaction'
+import { useCreateTransaction } from '@/api/hooks/transaction'
 import {
   Dialog,
   DialogClose,
@@ -25,7 +23,6 @@ import {
   DialogTitle,
   DialogTrigger,
 } from '@/components/ui/dialog'
-import { useAuthContext } from '@/contexts/auth'
 
 import { Button } from './ui/button'
 import { DatePicker } from './ui/date-picker'
@@ -53,18 +50,7 @@ const formSchema = z.object({
 })
 
 const AddTransactionButton = () => {
-  const queryClient = useQueryClient()
-  const { user } = useAuthContext()
-  const { mutateAsync: createTransaction } = useMutation({
-    mutationKey: ['createTransaction'],
-    mutationFn: (input) => TransactionService.create(input),
-    onSuccess: () => {
-      queryClient.invalidateQueries({
-        queryKey: getUserBalanceQueryKey({ userId: user.id }),
-        exact: false,
-      })
-    },
-  })
+  const { mutateAsync: createTransaction } = useCreateTransaction()
   const [dialogIsOpen, setDialogIsOpen] = useState(false)
   const form = useForm({
     resolver: zodResolver(formSchema),
